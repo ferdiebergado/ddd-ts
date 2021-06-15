e3f41b425407c7176089162a1ce206775a0037eb
ferdie <ferdiebergado@gmail.com>
2021-06-15 20:51:30 +0800

    Removed database export

--- src/shared/persistence/index.ts
+++ src/shared/persistence/index.ts
@@ -1,5 +1,3 @@
 import { MongodbConnection } from './mongodb/mongodb.connection'
-import { MongodbRepository } from './mongodb/mongodb.repository'
 
 export const dbConnection = new MongodbConnection()
-export const database = new MongodbRepository(dbConnection)
