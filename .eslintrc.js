a3c2c7c76805019d9aafd699540ad6c1cd4ee515
ferdie <ferdiebergado@gmail.com>
2021-06-17 20:59:01 +0800

    Added eslint airbnb plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['@typescript-eslint', 'jest'],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
     'plugin:jest/style',
+    'airbnb-typescript',
+    'prettier',
   ],
   settings: {
     jest: {
       version: 27,
     },
   },
-}
+};
