1e1aeee57950c6bcb7f6a0e7c672221a2b62f48e
ferdie <ferdiebergado@gmail.com>
2021-06-17 21:00:20 +0800

    Refactored

--- src/modules/user/domain/providers/hash.provider.ts
+++ src/modules/user/domain/providers/hash.provider.ts
@@ -1,25 +1,26 @@
-import { IHashProvider } from '../../application/hash.provider.interface'
-import crypto from 'crypto'
-import config from '../../../../config'
+/* eslint-disable class-methods-use-this */
+import crypto from 'crypto';
+import { IHashProvider } from '../../application/hash.provider.interface';
+import config from '../../../../config';
 
-export class CryptoHashProvider implements IHashProvider {
-  private _createHash(salt: string, plain: string) {
+export default class CryptoHashProvider implements IHashProvider {
+  protected createHash(salt: string, plain: string) {
     return crypto
       .createHmac(config.security.hash.algorithm, salt.normalize())
       .update(plain.normalize())
-      .digest('base64')
+      .digest('base64');
   }
 
   hash(plain: string): string {
-    const salt = crypto.randomBytes(16).toString('base64')
-    const hash = this._createHash(salt, plain)
-    return salt + '$' + hash
+    const salt = crypto.randomBytes(16).toString('base64');
+    const hash = this.createHash(salt, plain);
+    return `${salt}$${hash}`;
   }
 
   compare(hashed: string, plain: string): boolean {
-    const hashes = hashed.split('$')
-    const salt = hashes[0]
-    const hash = this._createHash(salt, plain)
-    return crypto.timingSafeEqual(Buffer.from(hash), Buffer.from(hashes[1]))
+    const hashes = hashed.split('$');
+    const salt = hashes[0];
+    const hash = this.createHash(salt, plain);
+    return crypto.timingSafeEqual(Buffer.from(hash), Buffer.from(hashes[1]));
   }
 }
