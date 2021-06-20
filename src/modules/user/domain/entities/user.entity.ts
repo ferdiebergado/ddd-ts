581226ca95fe1b9d558fc328e63f2b3d0342666b
ferdie <ferdiebergado@gmail.com>
2021-06-20 08:01:19 +0800

    Fixed all lonely ifs

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,149 +1,150 @@
-import { Entity, ValidationError } from '../../../../shared/entity'
-import { Messages } from '../../../../messages'
-import { isAlphaNum, isEmail } from '../../../../shared/utils/helpers'
+import { Entity, ValidationError } from '../../../../shared/entity';
+import { Messages } from '../../../../messages';
+import { isAlphaNum, isEmail } from '../../../../shared/utils/helpers';
 
 export interface IUser {
-  lastName: string
-  firstName: string
-  email: string
-  password: string
-  emailVerifiedAt?: string
-  isActive?: boolean
+  lastName: string;
+  firstName: string;
+  email: string;
+  password: string;
+  emailVerifiedAt?: string;
+  isActive?: boolean;
 }
 
 export interface UserEntity extends IUser, Entity {}
 
 export class User extends Entity implements IUser {
-  static readonly minNameLength = 2
-  static readonly maxStringLength = 150
-  static readonly minEmailLength = 6
-  static readonly minPasswordLength = 8
-  lastName = ''
-  firstName = ''
-  email = ''
-  password = ''
-  emailVerifiedAt = ''
-  isActive = true
+  static readonly minNameLength = 2;
+
+  static readonly maxStringLength = 150;
+
+  static readonly minEmailLength = 6;
+
+  static readonly minPasswordLength = 8;
+
+  lastName = '';
+
+  firstName = '';
+
+  email = '';
+
+  password = '';
+
+  emailVerifiedAt = '';
+
+  isActive = true;
 
   validate(): ValidationError[] {
-    const validationErrors: ValidationError[] = []
+    const validationErrors: ValidationError[] = [];
 
-    let field = 'lastName'
-
-    if (!this[field]) {
-      validationErrors.push({
-        field,
-        message: `${field} is required`,
-      })
-    } else {
-      if (!isAlphaNum(this[field])) {
-        validationErrors.push({
-          field,
-          message: `${field} should be alphanumeric only`,
-        })
-      } else {
-        if (this[field].length < User.minNameLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at least ${User.minNameLength} characters long`,
-          })
-        }
-        if (this[field].length > User.maxStringLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at most ${User.maxStringLength} characters long`,
-          })
-        }
-      }
-    }
-
-    field = 'firstName'
+    let field = 'lastName';
 
     if (!this[field]) {
       validationErrors.push({
         field,
         message: `${field} is required`,
-      })
+      });
+    } else if (!isAlphaNum(this[field])) {
+      validationErrors.push({
+        field,
+        message: `${field} should be alphanumeric only`,
+      });
     } else {
-      if (!isAlphaNum(this[field])) {
+      if (this[field].length < User.minNameLength) {
         validationErrors.push({
           field,
-          message: `${field} should be alphanumeric only`,
-        })
-      } else {
-        if (this[field].length < User.minNameLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at least ${User.minNameLength} characters long`,
-          })
-        }
-        if (this[field].length > User.maxStringLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at most ${User.maxStringLength} characters long`,
-          })
-        }
+          message: `${field} should be at least ${User.minNameLength} characters long`,
+        });
+      }
+      if (this[field].length > User.maxStringLength) {
+        validationErrors.push({
+          field,
+          message: `${field} should be at most ${User.maxStringLength} characters long`,
+        });
       }
     }
 
-    field = 'email'
+    field = 'firstName';
 
     if (!this[field]) {
       validationErrors.push({
         field,
         message: `${field} is required`,
-      })
+      });
+    } else if (!isAlphaNum(this[field])) {
+      validationErrors.push({
+        field,
+        message: `${field} should be alphanumeric only`,
+      });
     } else {
-      if (!isEmail(this[field])) {
+      if (this[field].length < User.minNameLength) {
         validationErrors.push({
           field,
-          message: Messages.EMAIL_INVALID,
-        })
-      } else {
-        if (this[field].length < User.minEmailLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at least ${User.minEmailLength} characters long`,
-          })
-        }
-        if (this[field].length > User.maxStringLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at most ${User.maxStringLength} characters long`,
-          })
-        }
+          message: `${field} should be at least ${User.minNameLength} characters long`,
+        });
+      }
+      if (this[field].length > User.maxStringLength) {
+        validationErrors.push({
+          field,
+          message: `${field} should be at most ${User.maxStringLength} characters long`,
+        });
       }
     }
 
-    field = 'password'
+    field = 'email';
 
     if (!this[field]) {
       validationErrors.push({
         field,
         message: `${field} is required`,
-      })
+      });
+    } else if (!isEmail(this[field])) {
+      validationErrors.push({
+        field,
+        message: Messages.EMAIL_INVALID,
+      });
     } else {
-      if (!isAlphaNum(this[field], true)) {
+      if (this[field].length < User.minEmailLength) {
         validationErrors.push({
           field,
-          message: `${field} should be alphanumeric only`,
-        })
-      } else {
-        if (this[field].length < User.minPasswordLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at least ${User.minPasswordLength} characters long`,
-          })
-        }
-        if (this[field].length > User.maxStringLength) {
-          validationErrors.push({
-            field,
-            message: `${field} should be at most ${User.maxStringLength} characters long`,
-          })
-        }
+          message: `${field} should be at least ${User.minEmailLength} characters long`,
+        });
+      }
+      if (this[field].length > User.maxStringLength) {
+        validationErrors.push({
+          field,
+          message: `${field} should be at most ${User.maxStringLength} characters long`,
+        });
       }
     }
 
-    return validationErrors
+    field = 'password';
+
+    if (!this[field]) {
+      validationErrors.push({
+        field,
+        message: `${field} is required`,
+      });
+    } else if (!isAlphaNum(this[field], true)) {
+      validationErrors.push({
+        field,
+        message: `${field} should be alphanumeric only`,
+      });
+    } else {
+      if (this[field].length < User.minPasswordLength) {
+        validationErrors.push({
+          field,
+          message: `${field} should be at least ${User.minPasswordLength} characters long`,
+        });
+      }
+      if (this[field].length > User.maxStringLength) {
+        validationErrors.push({
+          field,
+          message: `${field} should be at most ${User.maxStringLength} characters long`,
+        });
+      }
+    }
+
+    return validationErrors;
   }
 }
